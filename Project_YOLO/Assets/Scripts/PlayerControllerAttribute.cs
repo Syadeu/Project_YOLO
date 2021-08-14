775e464cf4611378d11f6670384f28ae38eca195
Syadeu <syadeu@g1playground.com>
2021-08-14 18:48:00 +0900

    u

---
+++ Project_YOLO/Assets/Scripts/PlayerControllerAttribute.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[AttributeAcceptOnly(typeof(ActorEntity))]
+//public class PlayerControllerAttribute : AttributeBase
+//{
+    
+//}
