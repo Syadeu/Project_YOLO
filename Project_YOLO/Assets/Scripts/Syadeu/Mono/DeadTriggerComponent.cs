c830e29bd3cf6598598d2bd49f6be77e9eacf0bc
Syadeu <syadeu@gmail.com>
2021-08-22 09:38:42 +0900

    죽는 트리거 추가

---
+++ Project_YOLO/Assets/Scripts/Syadeu/Mono/DeadTriggerComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Syadeu.Mono
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public sealed class DeadTriggerComponent : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.gameObject.CompareTag("Player")) return;
+
+            var pos = YOLOPresentationProvider.Instance.GameSystem.GetLastSavePosition();
+            PlayerController.instance.transform.position = pos;
+        }
+    }
+}
