f27e1d1d10e0fc4c5dead91790a9c847454bc6bf
Syadeu <syadeu@gmail.com>
2021-08-22 11:38:04 +0900

    작업

---
+++ Project_YOLO/Assets/Scripts/Syadeu/Presentation/Actions/EnablePlayerBoosterAction.cs
@@ -0,0 +1,14 @@
+﻿using Syadeu.Presentation;
+using Syadeu.Presentation.Entities;
+
+namespace Syadeu
+{
+    public sealed class EnablePlayerBoosterAction : YOLOActionBase
+    {
+        public override void Process(EntityData<IEntityData> entity)
+        {
+            PlayerController.instance.Booster.HaveBooster = true;
+            PlayerController.instance.Booster.boosterAvailable = true;
+        }
+    }
+}
---
+++ Project_YOLO/Assets/Scripts/Syadeu/Presentation/Actions/SoundtrackEmitter.cs
@@ -0,0 +1,16 @@
+﻿using Syadeu.FMOD;
+
+namespace Syadeu
+{
+    public sealed class SoundtrackEmitter : MonoManager<SoundtrackEmitter>
+    {
+        public override bool DontDestroy => true;
+
+        public override void OnInitialize()
+        {
+            FMODSystem.Play(0, 1);
+
+            base.OnInitialize();
+        }
+    }
+}
