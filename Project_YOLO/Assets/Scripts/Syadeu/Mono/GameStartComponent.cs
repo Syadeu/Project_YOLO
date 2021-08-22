196d928f4fc65d4fe4c3ecd9a601d8af51f1798e
Syadeu <syadeu@gmail.com>
2021-08-22 11:09:03 +0900

    씬플로우 수정

--- Project_YOLO/Assets/Scripts/Syadeu/Mono/GameStartComponent.cs
+++ Project_YOLO/Assets/Scripts/Syadeu/Mono/GameStartComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Syadeu.Presentation;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,13 +7,17 @@ namespace Syadeu.Mono
 {
     public sealed class GameStartComponent : MonoBehaviour
     {
+        public float loadingWait = 2;
+
         public float m_FakeWaitDelay = 0;
         public int m_StartDelay = 2;
 
         public UnityEvent UnityEvent;
 
-        private void Start()
+        private IEnumerator Start()
         {
+            yield return new WaitForSeconds(loadingWait);
+
             PresentationSystem<SceneSystem>.System.LoadScene(0, m_FakeWaitDelay, m_StartDelay);
             UnityEvent?.Invoke();
         }
